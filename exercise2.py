f39705b041a0f5808a8057df13196c3fa6e97c3e
DeannaWong <deannawong@hotmail.ca>
2015-11-27 17:11:12 -0500

    Minor changes to code

--- exercise2.py
+++ exercise2.py
@@ -36,7 +36,6 @@ COUNTRIES = None
 with open("test_jsons/test_returning_citizen.json", "r") as entry_record_reader:
     entry_record_contents = entry_record_reader.read()
 
-
 #def entry_record_check():
 #    for row in entry_record_contents:
 #        if "passport: "
