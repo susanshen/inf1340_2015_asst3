b057cfac6c9c4eb04e6bcf834b468dc99133e085
alyhashahrukh <alyha.shahrukh@mail.utoronto.ca>
2015-12-05 12:18:32 -0500

    test cases update

--- test_exercise2.py
+++ test_exercise2.py
@@ -34,9 +34,26 @@ def entry_record_check_complete(input_file):
     assert decide("test_returning_citizen.json", "countries.json") ==\
         ["Accept", "Accept", "Quarantine"]
 
+
 def entry_record_check_incomplete(input_file):
     """
     First entry record is incomplete.
+    """
+    assert decide("test_returning_citizen.json", "countries.json") ==\
+        ["Reject", "Accept", "Quarantine"]
+
+
+def unknown_location(input_file):
+    """
+    Entry record location is unknown.
+    """
+    assert decide("test_returning_citizen.json", "countries.json") ==\
+        ["Reject", "Accept", "Quarantine"]
+
+
+def medical_advisory(input_file):
+    """
+    Traveling through country with medical advisory.
     """
     assert decide("test_returning_citizen.json", "countries.json") ==\
         ["Reject", "Accept", "Quarantine"]
