f75dbb90523e75f2983a40dc8c92fa5cffc91631
alyhashahrukh <alyha.shahrukh@mail.utoronto.ca>
2015-12-10 13:17:49 -0500

    test cases update

--- test_exercise2.py
+++ test_exercise2.py
@@ -14,7 +14,7 @@ __status__ = "Prototype"
 import pytest
 import os
 from exercise2 import decide
-#from exercise2 import visit_visa_check
+#from exercise2 import medical_advisory_check
 
 DIR = "test_jsons/"
 os.chdir(DIR)
@@ -36,9 +36,29 @@ def test_entry_record_completeness():
         ["Reject", "Accept", "Quarantine"]
 
 
+def test_location_existence():
+    """
+    Location mentioned in entry record is unknown
+    """
+    assert decide("test_returning_citizen_3.json", "countries.json") ==\
+        ["Accept", "Accept", "Reject"]
 
 
-#def medical_advisory():
 
- #   assert visit_visa_check("test_returning_citizen.json", "countries.json") ==\
-  #      [True]
+# Problem - testing decide function
+# it still passes even when I insert Reject in one of the outputs
+def check_visa_validation():
+    """
+    Reason for entry is to visit and the visitor has a passport from a country from which a visitor visa is required
+    the traveller has a valid visa.
+    """
+    assert decide("test_returning_citizen_4.json", "countries.json") ==\
+        ["Accept", "Accept", "Quarantine"]
+
+
+# Problem - tried to test the medical_advisory function
+# it still passes even when I insert Reject in one of the outputs
+def medical_advisory():
+
+    assert medical_advisory_check("test_returning_citizen.json", "countries.json") ==\
+        ["Accept", "Accept", "Quarantine"]
