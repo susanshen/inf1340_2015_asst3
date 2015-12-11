6054636f99aabf2ab1bd545fdc3846be5a27eb86
DeannaWong <deannawong@hotmail.ca>
2015-12-10 22:45:02 -0500

    Fixed format of code

--- exercise2.py
+++ exercise2.py
@@ -51,6 +51,8 @@ COUNTRIES = None
 #####################
 # HELPER FUNCTIONS ##
 #####################
+
+
 def is_more_than_x_years_ago(x, date_string):
     """
     Check if date is less than x years ago.
@@ -64,13 +66,8 @@ def is_more_than_x_years_ago(x, date_string):
     x_years_ago = now.replace(year=now.year - x)
     date = datetime.datetime.strptime(date_string, '%Y-%m-%d')
 
-    
     global x_years_flag
     x_years_flag = (date - x_years_ago).total_seconds() > 0
-##    if x_years_flag:
-##        print('True',(date - x_years_ago).total_seconds())
-##    else:
-##        print('False',(date - x_years_ago).total_seconds())
 
 
 def valid_visa_format(visa_code):
@@ -97,8 +94,6 @@ def valid_date_format(date_string):
     :param date_string: date to be checked
     :return: Boolean True if the format is valid, False otherwise
     """
-
-    # TESTED & WORKING
     global valid_date_format_flag
     valid_date_format_regex = re.compile(r'\d\d\d\d-\d\d-\d\d')
     valid_date_format_match = valid_date_format_regex.search(date_string)
@@ -116,7 +111,6 @@ def valid_passport_format(passport_number):
     :param passport_number: alpha-numeric string
     :return: Boolean; True if the format is valid, False otherwise
     """
-    # TESTED & WORKING
     global valid_passport_format_flag
     valid_passport_regex = re.compile(r'\w\w\w\w\w-\w\w\w\w\w-\w\w\w\w\w-\w\w\w\w\w-\w\w\w\w\w')
     valid_passport_match = valid_passport_regex.search(passport_number)
@@ -126,18 +120,18 @@ def valid_passport_format(passport_number):
     else:
         valid_passport_format_flag = False
         return False
-########
+
+
 def entry_record_check(input_file):
     with open(input_file, "r") as entry_record_reader:
         entry_record_contents = entry_record_reader.read()
         test_return = json.loads(entry_record_contents)
     list = []
     i = 0
-    while (i < len(test_return) ):
+    while i < len(test_return):
         cnt_flag = 'F'
         
-    # initializing all control flags to 'Accept' first
-
+        # initializing all control flags to 'Accept' first
         for rf_key in REQUIRED_FIELDS:
             for key1 in test_return:
                 if rf_key not in key1:
@@ -179,10 +173,11 @@ def entry_record_check(input_file):
             list.append("Reject")
         else:
             list.append("Accept")
-        i = i + 1
+        i += 1
         continue
     return list
-    
+
+
 def location_check(input_file, countries_file):
 
     with open(input_file, "r") as entry_record_reader:
@@ -209,6 +204,7 @@ def location_check(input_file, countries_file):
             list.append("Accept")
     return list
 
+
 def medical_advisory_check(input_file, countries_file):
     with open(input_file, "r") as entry_record_reader:
         entry_record_contents = entry_record_reader.read()
@@ -220,7 +216,7 @@ def medical_advisory_check(input_file, countries_file):
 
     list = []
     i = 0
-    while (i < len(test_return) ):
+    while i < len(test_return):
         cnt_flag = 'F'
         from_country = (test_return[i]['from']['country'])
         for key in COUNTRIES:
@@ -254,6 +250,7 @@ def location_check1(input_data, countries_file):
     else:
         location_check_flag = 'A'
 
+
 def visit_visa_check(input_data, countries_file):
     """
     Checks if country requires visa
@@ -272,6 +269,7 @@ def visit_visa_check(input_data, countries_file):
             else:
                 visit_visa_check_flag = False
 
+
 def medical_advisory_check1(input_data, countries_file):
     """
     Checks if traveller is coming from a country with a medical advisory
@@ -288,10 +286,8 @@ def medical_advisory_check1(input_data, countries_file):
         if key.lower() == input_data.lower():
             if countries_file[key]['medical_advisory'] == "":
                 medical_advisory_check_flag = 'A'
-##                print('medical_advisory_check_flag',medical_advisory_check_flag)
             else:
                 medical_advisory_check_flag = 'Q'
-##                print('medical_advisory_check_flag',medical_advisory_check_flag)
 
 
 def decide(input_file, countries_file):      
@@ -306,7 +302,6 @@ def decide(input_file, countries_file):
     :return: List of strings. Possible values of strings are:
         "Accept", "Reject", and "Quarantine"
     """
-
     with open(input_file, "r") as entry_record_reader:
         entry_record_contents = entry_record_reader.read()
         test_return = json.loads(entry_record_contents)
@@ -317,13 +312,12 @@ def decide(input_file, countries_file):
 
         list = []
         i = 0
-        while (i < len(test_return) ):
+        while i < len(test_return):
             cnt_flag = 'A'
             print(i)
             print(cnt_flag)
             
-        # initializing all control flags to 'Accept' first
-
+            # initializing all control flags to 'Accept' first
             for rf_key in REQUIRED_FIELDS:
                 for key1 in test_return:
                     if rf_key not in key1:
@@ -340,31 +334,32 @@ def decide(input_file, countries_file):
             home_region = (test_return[i]['home']['region'])
             home_city = (test_return[i]['home']['city'])
             if birth_date == "" and cnt_flag != 'R':
-                print('1-',cnt_flag)
+                print('1-', cnt_flag)
                 cnt_flag = 'R'
-            print('1-',cnt_flag)
+            print('1-', cnt_flag)
             if passport == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('2-',cnt_flag)
-            print('2-',cnt_flag)
+                print('2-', cnt_flag)
+            print('2-', cnt_flag)
+
             # Checking format of passport
             valid_passport_format(passport)
             if valid_passport_format_flag == False and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('3-',cnt_flag)
-            print('3-',cnt_flag)
+                print('3-', cnt_flag)
+            print('3-', cnt_flag)
             if last_name == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('4-',cnt_flag)
-            print('4-',cnt_flag)
+                print('4-', cnt_flag)
+            print('4-', cnt_flag)
             if first_name == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('5-',cnt_flag)
-            print('5-',cnt_flag)
+                print('5-', cnt_flag)
+            print('5-', cnt_flag)
             if entry_reason == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('6-',cnt_flag)
-            print('6-',cnt_flag)
+                print('6-', cnt_flag)
+            print('6-', cnt_flag)
             if entry_reason.lower() == 'visiting':
                 if 'visa_code' in (test_return[i]) and 'visa_date' in (test_return[i]):
                     visa_code = (test_return[i]['visa_code'])
@@ -374,66 +369,70 @@ def decide(input_file, countries_file):
                         valid_visa_format(visa_code)
                         if valid_visa_format_flag == False and cnt_flag != 'R':
                             cnt_flag = 'R'
-                            print('7-',cnt_flag)
-                        print('7-',cnt_flag)
+                            print('7-', cnt_flag)
+                        print('7-', cnt_flag)
                         valid_date_format(visa_date)
                         if valid_date_format_flag == False and cnt_flag != 'R':
                             cnt_flag = 'R'
-                            print('8-',cnt_flag)
+                            print('8-', cnt_flag)
                         else:
                             is_more_than_x_years_ago(2,visa_date)
                             if x_years_flag == False and cnt_flag != 'R':
                                 cnt_flag = 'R'
-                                print('9-',cnt_flag)
-                            print('9-',cnt_flag)
-                        print('8-',cnt_flag)
+                                print('9-', cnt_flag)
+                            print('9-', cnt_flag)
+                        print('8-', cnt_flag)
                 else:
                     cnt_flag = 'R'
-                    print('10-',cnt_flag)
-                print('10-',cnt_flag)
+                    print('10-', cnt_flag)
+                print('10-', cnt_flag)
             if from_country == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('11-',cnt_flag)
-            print('11-',cnt_flag)
+                print('11-', cnt_flag)
+            print('11-', cnt_flag)
             if from_region == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('12-',cnt_flag)
-            print('12-',cnt_flag)
+                print('12-', cnt_flag)
+            print('12-', cnt_flag)
             if from_city == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('13-',cnt_flag)
-            print('13-',cnt_flag)
+                print('13-', cnt_flag)
+            print('13-', cnt_flag)
+
             # Decision for Question 2
             location_check1(from_country, COUNTRIES)
             if location_check_flag == 'R' and cnt_flag != 'R':
                 cnt_flag = 'R'
                 print('14-',cnt_flag)
             print('14-',cnt_flag)
+
             # Decision for Question 5
             medical_advisory_check1(from_country, COUNTRIES)
             if medical_advisory_check_flag == 'Q' and cnt_flag != 'R':
                 cnt_flag = 'Q'
-                print('15-',cnt_flag)
-            print('15-',cnt_flag)
+                print('15-', cnt_flag)
+            print('15-', cnt_flag)
             if home_country == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('16-',cnt_flag)
-            print('16-',cnt_flag)
+                print('16-', cnt_flag)
+            print('16-', cnt_flag)
             if home_region == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('17-',cnt_flag)
-            print('17-',cnt_flag)
+                print('17-', cnt_flag)
+            print('17-', cnt_flag)
             if home_city == "" and cnt_flag != 'R':
                 cnt_flag = 'R'
-                print('18-',cnt_flag)
-            print('18-',cnt_flag)
+                print('18-', cnt_flag)
+            print('18-', cnt_flag)
+
             # Decision for Question 3
             if home_country.upper() == 'KAN' and cnt_flag == 'F':
                 cnt_flag = 'A'
-                print('19-',cnt_flag)
-            print('19-',cnt_flag)
+                print('19-', cnt_flag)
+            print('19-', cnt_flag)
+
             # If any of the above checks return 'Reject' the entry record will be rejected
-            if cnt_flag != 'F' :
+            if cnt_flag != 'F':
                 if cnt_flag == 'R':
                     list.append("Reject")
                 elif cnt_flag == 'Q':
@@ -441,12 +440,6 @@ def decide(input_file, countries_file):
                 else:
                     list.append("Accept")
                 cnt_flag = 'F'
-            i = i + 1
+            i += 1
             continue 
-        return  list
-    
-##if __name__ == "__main__":
-##    # execute only if run as a script
-##    main()
-##p = location_check('LUG', COUNTRIES)
-##print(p)
+        return list
